a236d1908712405bc80f35b809f142cf504224b6
clifford <kpoku823@gmail.com>
2026-03-23 05:19:37 +0000

    Add vulnerable endpoint for testing

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const { exec } = require('child_process');
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Secure Pipeline Lab Running 🚀');
+});
+
+// 🚨 VULNERABLE ENDPOINT
+app.get('/run', (req, res) => {
+  const userInput = req.query.cmd;
+
+  exec(userInput, (error, stdout, stderr) => {
+    if (error) {
+      return res.send(`Error: ${error.message}`);
+    }
+    res.send(`Output: ${stdout}`);
+  });
 });
 
 app.listen(3000, () => {
