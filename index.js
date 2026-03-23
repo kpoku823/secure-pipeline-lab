3b64c5bd56203429e62f7c3937b6d80f6dd6f3f5
clifford <kpoku823@gmail.com>
2026-03-23 17:10:42 +0000

    update js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const { exec } = require('child_process');
+const rateLimit = require('express-rate-limit');
+
+
 
 const app = express();
+
+// rate limiter
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 5
+});
+
+app.use('/run', limiter);
+
 
 app.get('/', (req, res) => {
   res.send('Secure Pipeline Lab Running 🚀');
 });
 
-// 🚨 VULNERABLE ENDPOINT
+/*// 🚨 VULNERABLE ENDPOINT
 app.get('/run', (req, res) => {
   const userInput = req.query.cmd;
 
@@ -18,6 +30,26 @@ app.get('/run', (req, res) => {
     res.send(`Output: ${stdout}`);
   });
 });
+//*/
+
+//VULNERABLE ENDPOINT FIXED
+const allowedCommands = ['date', 'uptime'];
+
+app.get('/run', (req, res) => {
+  const userInput = req.query.cmd;
+
+  if (!allowedCommands.includes(userInput)) {
+    return res.status(400).send('Invalid command');
+  }
+
+  exec(userInput, (error, stdout, stderr) => {
+    if (error) {
+      return res.send(`Error: ${error.message}`);
+    }
+    res.send(`Output: ${stdout}`);
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
